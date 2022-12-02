09495761d6f5dce483ee24b3c16077478cb05b8d
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-12-02 16:04:32 +0000

    Day 2

---
+++ day-2/day-2.py
@@ -0,0 +1,46 @@
+convert_moves = {
+    "X": "A",
+    "Y": "B",
+    "Z": "C"
+}
+
+win_conditions = {
+    "A": "B",
+    "B": "C",
+    "C": "A"
+}
+
+move_points = {
+    "A": 1,
+    "B": 2,
+    "C": 3
+}
+
+def calculate_move_points(moves):
+    if moves[0] == moves[1]: return 3
+    return 6 if moves[1] == win_conditions[moves[0]] else 0
+
+def split_line(line):
+    moves = line.strip("\n").split(" ")
+    moves[1] = convert_moves[moves[1]]
+    return moves
+
+def generate_move(line):
+    moves = line.strip("\n").split(" ")
+    if moves[1] == "Y": moves[1] = moves[0]
+    elif moves[1] == "Z": moves[1] = win_conditions[moves[0]]
+    else: moves[1] = dict([reversed(i) for i in win_conditions.items()])[moves[0]]
+
+    return moves
+
+def calculate_round_points(line, ver):
+    moves = split_line(line) if ver == 1 else generate_move(line)
+    return calculate_move_points(moves) + move_points[moves[1]]
+
+# Part 1
+with open("day-2/input.txt", "r") as f:
+    print(sum([calculate_round_points(line, 1) for line in f]))
+
+# Part 2
+with open("day-2/input.txt", "r") as f:
+    print(sum([calculate_round_points(line, 2) for line in f]))
