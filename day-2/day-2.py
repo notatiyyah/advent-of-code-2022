358a4b76bea3dba64c714d70082067bb55a48603
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-12-02 16:10:38 +0000

    Formtting + comments

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -16,7 +16,7 @@ move_points = {
     "C": 3
 }
 
-def calculate_move_points(moves):
+def calculate_if_win_points(moves): # ?? Can't think of better name
     if moves[0] == moves[1]: return 3
     return 6 if moves[1] == win_conditions[moves[0]] else 0
 
@@ -25,17 +25,17 @@ def split_line(line):
     moves[1] = convert_moves[moves[1]]
     return moves
 
-def generate_move(line):
+def generate_moves(line):
     moves = line.strip("\n").split(" ")
     if moves[1] == "Y": moves[1] = moves[0]
     elif moves[1] == "Z": moves[1] = win_conditions[moves[0]]
-    else: moves[1] = dict([reversed(i) for i in win_conditions.items()])[moves[0]]
+    else: moves[1] = dict([reversed(i) for i in win_conditions.items()])[moves[0]] # Not particularly happy about this lol
 
     return moves
 
 def calculate_round_points(line, ver):
-    moves = split_line(line) if ver == 1 else generate_move(line)
-    return calculate_move_points(moves) + move_points[moves[1]]
+    moves = split_line(line) if ver == 1 else generate_moves(line)
+    return calculate_if_win_points(moves) + move_points[moves[1]]
 
 # Part 1
 with open("day-2/input.txt", "r") as f:
